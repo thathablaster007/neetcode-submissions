d115feced245dce97e62df7caa75584ddfd64e11
NEERAV AIYAPPA M <neerav.aiyappa@gmail.com>
2026-08-18 14:09:40 +0530

    Add: sqrtx - submission-3

---
+++ Data Structures & Algorithms/sqrtx/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l,r = 0,(x//2)+1
+        res = r
+        while l <= r:
+            m = (l + r) // 2
+            if m*m == x:
+                return m
+            if m*m > x:
+                r = m - 1
+            else:
+                res = m
+                l = m + 1
+        return res
+
+        
